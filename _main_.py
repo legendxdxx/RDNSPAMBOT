f18f897404a686801628047ad21869b44ca20550
legendxdxx <105597001+legendxdxx@users.noreply.github.com>
2022-06-24 20:22:27 +0530

    Update _main_.py

--- _main_.py
+++ _main_.py
@@ -3,16 +3,16 @@ import sys
 from sys import argv
 import glob
 from pathlib import Path
-from MightyXSpam.utils import load_plugins
+from RDNSPAMBOT.utils import load_plugins
 import logging
 from telethon import events
-from . import Mig, Mig2, Mig3, Mig4, Mig5, Mig6, Mig7, Mig8, Mig9, Mig10
+from . import Rdn, Rdn2, Rdn3, Rdn4, Rdn5, Rdn6, Rdn7, Rdn8, Rdn9, Rdn10
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
 
-path = "MightyXSpam/plugins/*.py"
+path = "RDNSPAMBOT/plugins/*.py"
 files = glob.glob(path)
 for name in files:
     with open(name) as a:
@@ -21,34 +21,34 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
 
 print("Mighty X BotSpam Successfully Deployed !!")
-print("Enjoy..!  Do Visit @MightyXSupport")
+print("Enjoy..!  Support @RMWNETWORK ")
 
 if __name__ == "__main__":
-    Mig.run_until_disconnected()
+    Rdn.run_until_disconnected()
     
 if __name__ == "__main__":
-    Mig2.run_until_disconnected()
+    Rdn2.run_until_disconnected()
 
 if __name__ == "__main__":
-    Mig3.run_until_disconnected()
+    Rdn3.run_until_disconnected()
     
 if __name__ == "__main__":
-    Mig4.run_until_disconnected()
+    Rdn4.run_until_disconnected()
 
 if __name__ == "__main__":
-    Mig5.run_until_disconnected()
+    Rdn5.run_until_disconnected()
     
 if __name__ == "__main__":
-    Mig6.run_until_disconnected()
+    Rdn6.run_until_disconnected()
     
 if __name__ == "__main__":
-    Mig7.run_until_disconnected()
+    Rdn7.run_until_disconnected()
 
 if __name__ == "__main__":
-    Mig8.run_until_disconnected()
+    Rdn8.run_until_disconnected()
     
 if __name__ == "__main__":
-    Mig9.run_until_disconnected()
+    Rdn9.run_until_disconnected()
 
 if __name__ == "__main__":
-    Mig10.run_until_disconnected()    
+    Rdn10.run_until_disconnected()    
