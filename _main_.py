fd949af6df2cefe0b43b3c8b3d086dd1e2553093
legendxdxx <105597001+legendxdxx@users.noreply.github.com>
2022-06-24 20:23:15 +0530

    Update _main_.py

--- _main_.py
+++ _main_.py
@@ -20,7 +20,7 @@ for name in files:
         plugin_name = patt.stem
         load_plugins(plugin_name.replace(".py", ""))
 
-print("Mighty X BotSpam Successfully Deployed !!")
+print("RDNSPAMBOTS Successfully Deployed !!")
 print("Enjoy..!  Support @RMWNETWORK ")
 
 if __name__ == "__main__":
