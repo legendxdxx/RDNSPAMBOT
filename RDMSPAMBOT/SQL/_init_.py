62198d27538314bcedee5b4c6a8726792c684fec
legendxdxx <105597001+legendxdxx@users.noreply.github.com>
2022-06-25 09:13:39 +0530

    Update _init_.py

--- RDMSPAMBOT/SQL/_init_.py
+++ RDMSPAMBOT/SQL/_init_.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 # the secret configuration specific things
-from MightyXSpam import DB_URI
+from RDMSPAMBOT import DB_URI
 
 
 def start() -> scoped_session:
